fd78c5b67fb0e43481e1c783babe98c85276c058
Long Tran <longtran2186@gmail.com>
2017-12-12 16:41:42 +0100

    indeoendency testing

--- model.r
+++ model.r
@@ -2,7 +2,7 @@
 library(bnlearn)
 library(Rgraphviz)
 
-root_path = 'D:\\OneDrive\\Documenti\\Radboud\\2017\\Bayesian Networks\\Assignment 1\\src\\'
+root_path = 'D:\\PyCharm\\BayesNet_Movies\\Bayesian_github\\BN-2017'
 data_path = file.path(root_path, 'data')
 
 # Read CSV into R
@@ -69,6 +69,75 @@ fitted <- bn.fit(defined_net, data=t)
 
 # TODO test independences
 # Look into bnlearn::ci.test or chisq.test
+implications <- list(c("revenue","budget","major","genre","cast_popularity"),
+                     c("revenue","budget","genre","us","cast_popularity"),
+                     c("revenue","budget","movie_popularity"),
+                     c("revenue","genre","movie_popularity"),
+                     c("revenue","major","us","genre","budget"),
+                     c("revenue","major","cast_popularity","genre","us"),
+                     c("revenue","major","movie_popularity"),
+                     c("revenue","us","movie_popularity"),
+                     c("revenue","cast_popularity","movie_popularity"),
+                     c("revenue","community_vote","movie_popularity"),
+                     c("revenue","critics_vote","movie_popularity"),
+                     c("revenue","community_count","movie_popularity"),
+                     c("revenue","critics_count","movie_popularity"),
+                     c("budget","us","major"),
+                     c("budget","movie_popularity","genre","cast_popularity","us"),
+                     c("budget","movie_popularity","genre","cast_popularity","major"),
+                     c("budget","community_vote"),
+                     c("budget","critics_vote"),
+                     c("budget","community_count","movie_popularity"),
+                     c("budget","community_count","genre","cast_popularity","us"),
+                     c("budget","community_count","genre","cast_popularity","major"),
+                     c("budget","critics_count","movie_popularity"),
+                     c("budget","critics_count","genre","cast_popularity","us"),
+                     c("budget","critics_count","genre","cast_popularity","major"),
+                     c("genre","us","major"),
+                     c("genre","cast_popularity","budget"),
+                     c("genre","community_vote"),
+                     c("genre","critics_vote"),
+                     c("genre","community_count","movie_popularity"),
+                     c("genre","critics_count","movie_popularity"),
+                     c("major","movie_popularity","genre","cast_popularity","us"),
+                     c("major","movie_popularity","genre","us","budget"),
+                     c("major","cast_popularity","budget"),
+                     c("major","community_vote"),
+                     c("major","critics_vote"),
+                     c("major","community_count","movie_popularity"),
+                     c("major","community_count","genre","cast_popularity","us"),
+                     c("major","community_count","genre","us","budget"),
+                     c("major","critics_count","movie_popularity"),
+                     c("major","critics_count","genre","cast_popularity","us"),
+                     c("major","critics_count","genre","us","budget"),
+                     c("us","cast_popularity","budget"),
+                     c("us","cast_popularity","major"),
+                     c("us","community_vote"),
+                     c("us","critics_vote"),
+                     c("us","community_count","movie_popularity"),
+                     c("us","critics_count","movie_popularity"),
+                     c("cast_popularity","community_vote"),
+                     c("cast_popularity","critics_vote"),
+                     c("cast_popularity","community_count","movie_popularity"),
+                     c("cast_popularity","critics_count","movie_popularity"),
+                     c("community_vote","critics_vote"),
+                     c("community_vote","community_count","movie_popularity"),
+                     c("community_vote","critics_count","movie_popularity"),
+                     c("critics_vote","community_count","movie_popularity"),
+                     c("critics_vote","critics_count","movie_popularity"),
+                     c("community_count","critics_count","movie_popularity"))
+count <- 1
+citest_list <- list()
+for (line in implications)
+{
+  citest_list[[paste(count)]] <- ci.test(x = line[1] , y = line[2], z = c(line[-1:-2]), data = t)
+  count <- count+1
+}
+
+for (i in 1:length(citest_list))
+{
+  cat ("\nnumber ",i," : ",citest_list[[i]]$p.value, " --- ", citest_list[[i]]$data.name)
+}
 
 # TODO inference
 # - predict a movie's popularity
