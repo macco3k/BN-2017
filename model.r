facae8e8e2adae98f0d1971ed676ec2c1274e7dd
Joost Besseling <joost.pbesseling@gmail.com>
2017-12-20 17:07:11 +0100

    calculate the missing correlations

--- model.r
+++ model.r
@@ -3,7 +3,9 @@ library(bnlearn)
 library(Rgraphviz)
 
 root_path = 'D:\\Documents\\Github\\BN-2017'
+root_path = '~/Documents/RU/BN/BN-2017'
 data_path = file.path(root_path, 'data')
+
 source(file.path(root_path, 'testable_implications_v3a.r'))
 source(file.path(root_path, 'helper.r'))
 
@@ -18,16 +20,17 @@ defined_net = model2network(defined_net_string)
 graphviz.plot(defined_net)
 
 t <- read.csv(file.path(data_path, 'train.csv'))
+
 t <- t[c('major', 
-         'macro_genre', 
-         'budget_binned',
+         'genre', 
+         'budget',
          'us', 
-         'cast_popularity_binned', 
-         'vote_average_binned', 
-         'vote_count_binned',
-         'critics_vote_binned',
-         'critics_count_binned',
-         'roi_binned')]
+         'cast_popularity', 
+         'community_vote', 
+         'community_count',
+         'critics_vote',
+         'critics_count',
+         'roi')]
          # 'revenue_binned',
          # 'popularity_binned')]
 
@@ -41,20 +44,41 @@ count <- 1
 citest_list <- c()
 citest.p.value <- c()
 citest.names <- c()
+citest.rmsea <- c()
 
 for (i in implications)
 {
-    print(i)
+    # print(i)
     test <- ci.test(x = i[1] , y = i[2], z = c(i[-1:-2]), data = t, test="x2")
     citest_list[count] <- test
     citest.p.value[count] <- test$p.value
+    
+    # not that some numbers will be NaN, this is, according to some webstie overfit of the data
+    # so we can safely ignore it
+    
+    citest.rmsea[count] <- max(sqrt( ((test$statistic/test$parameter) - 1 )/(3000 - 1)),0)
     citest.names[count] = test$data.name
+
+    
+    
     count <- count+1
 }
 
+rmsea = function(test) {
+  max(sqrt( ((test$statistic/test$parameter) - 1 )/(3000 - 1)),0)
+}
+
 df <- data.frame(names=citest.names, p.value=citest.p.value)
+df <- data.frame(names=citest.names, RMSEA=citest.rmsea)
+
 # todo plot this with points and maybe prettier labels
+op <- par(mar=c(11,4,4,2)) # the 10 allows the names.arg below the barplot
+
 plot(df, las=2, xlab='')
+rm(op)
+
+# run the following code: to calculate the rmseas with higher dan 0.05
+df[df["RMSEA"] > 0.05,]
 
 # TODO inference
 # - predict a movie's popularity
