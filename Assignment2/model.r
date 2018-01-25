8caa91850df688ec99d7040bc4ec995b61c12808
Daniele Maccari <daniele.maccari@gmail.com>
2018-01-25 18:19:16 +0100

    Some experiment with pc

--- Assignment2/model.r
+++ Assignment2/model.r
@@ -1,10 +1,11 @@
 # require(gRain);
+library(pcalg)
 library(bnlearn)
 library(Rgraphviz)
 
 # Change this to yours!
-# root_path = 'D:\\Documents\\Github\\BN-2017'
-root_path = '~/Documents/RU/BN/BN-2017'
+root_path = 'D:\\Documents\\Github\\BN-2017'
+# root_path = '~/Documents/RU/BN/BN-2017'
 data_path = file.path(root_path, 'data')
 
 # source(file.path(root_path, 'helper.r'))
@@ -25,11 +26,16 @@ t <- t[c('major',
          # 'revenue_binned',
          # 'popularity_binned')]
 
+nlev <- c(2,4,3,2,3,3,3,3,3,4)
 names(t) = c('major','genre','budget','us','cast_popularity','community_vote', 'community_count', 'critics_vote', 'critics_count', 'roi')
 
+graph.par(list(nodes=list(fontsize=36)))
 
-hiton_net = si.hiton.pc(t, undirected = FALSE, test="x2")
-graphviz.plot(net)
+suffStat = list(dm=t, nlev=nlev, adaptDF=FALSE)
+pc_net = pc(suffStat, indepTest = disCItest, alpha=0.05, labels=colnames(t), verbose=TRUE)
+
+hiton_net = si.hiton.pc(t, undirected = FALSE, test="mi", debug = TRUE)
+graphviz.plot(hiton_net)
 
 tabu_net = tabu(t)
 graphviz.plot(tabu_net)
