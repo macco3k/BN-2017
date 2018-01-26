16b51be3c6b6daa538508cd0825b21fc5a1bed60
Daniele Maccari <daniele.maccari@gmail.com>
2018-01-26 18:22:21 +0100

    Added a couple of function for counting diff in vstructs and arcs (wip)

--- Assignment2/model.r
+++ Assignment2/model.r
@@ -2,6 +2,21 @@
 library(pcalg)
 library(bnlearn)
 library(Rgraphviz)
+library(foreach)
+
+count_vstructs <- function(g1, g2){
+  v1 <- vstructs(g1)
+  v2 <- vstructs(g2)
+  
+  # compute the intersection
+  v <- rbind(v1,v2)
+  v <- v[duplicated(v), , drop=FALSE]
+  
+  if(dim(v1)[1] > dim(v2)[1])
+    dim(v1)[1] - dim(v)[1]
+  else
+    dim(v2)[1] - dim(v)[1]
+}
 
 # Change this to yours!
 root_path = 'D:\\Documents\\Github\\BN-2017'
@@ -29,12 +44,24 @@ t <- t[c('major',
 nlev <- c(2,4,3,2,3,3,3,3,3,4)
 names(t) = c('major','genre','budget','us','cast_popularity','community_vote', 'community_count', 'critics_vote', 'critics_count', 'roi')
 
+t_pc <- data.frame(t)
+for(name in names(t_pc)){
+  f <- as.factor(t_pc[[name]])
+  levels(f) <- 1:length(levels(f))
+  f <- as.numeric(f)-1
+  t_pc[name] <- f
+}
+
 graph.par(list(nodes=list(fontsize=36)))
 
-suffStat = list(dm=t, nlev=nlev, adaptDF=FALSE)
-pc_net = pc(suffStat, indepTest = disCItest, alpha=0.05, labels=colnames(t), verbose=TRUE)
+suffStat = list(dm=t_pc, nlev=nlev, adaptDF=FALSE, labels=names)
+pc_net = pc(suffStat, indepTest = chisq.test, alpha=0.05, labels=colnames(t), verbose=TRUE)
+plot(pc_net, main = "")
 
-hiton_net = si.hiton.pc(t, undirected = FALSE, test="mi", debug = TRUE)
+hiton_net = si.hiton.pc(t, undirected = FALSE, test="mi", debug = TRUE, alpha=0.05)
+graphviz.plot(hiton_net)
+
+hiton_hc_net = hc(t, restart = 1000, optimized = TRUE, whitelist = hiton_net$arcs)
 graphviz.plot(hiton_net)
 
 tabu_net = tabu(t)
